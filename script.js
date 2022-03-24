c008f76078c7a8ee853bf3da21458a6bc06ce67f
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2022-03-24 15:59:05 -0400

    deleted console.log

--- script.js
+++ script.js
@@ -43,7 +43,6 @@ function showTemp(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  console.log(response.data);
   getForecast(response.data.coord);
 }
 
@@ -101,10 +100,9 @@ function formatDay(timestamp) {
   return days[day];
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c5b46e313ac60a38d46e9623287e0a7d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -140,7 +138,6 @@ function convertToFahrenheit(event) {
   feelsLike.innerHTML = `${currentFeelsLike}°F`;
 }
 function displayForecast(response) {
-  console.log(response.data.daily);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
@@ -200,11 +197,10 @@ function showCoord(position) {
   let lon = position.coords.longitude;
   let apiKey = "c5b46e313ac60a38d46e9623287e0a7d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-  console.log(apiUrl);
+
   axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemp);
 }
 function displayTemp(response) {
-  console.log(response.data);
   let temperature = document.querySelector("#temperature");
   let currentTemp = Math.round(response.data.main.temp);
   temperature.innerHTML = `${currentTemp}°F`;
